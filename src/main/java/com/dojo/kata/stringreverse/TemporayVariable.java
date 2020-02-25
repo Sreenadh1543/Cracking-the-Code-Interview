c9a3736b90b41a9a10a01af3bd94fcb7dfed3d9a
sreenadh1543@gmail.com <sreenadh1543@gmail.com>
2020-02-25 19:24:34 +0100

    StringReverse : Using one array with a temporary variable

---
+++ src/main/java/com/dojo/kata/stringreverse/TemporayVariable.java
@@ -0,0 +1,17 @@
+package com.dojo.kata.stringreverse;
+
+public class TemporayVariable {
+
+    public static void main(String[] args) {
+
+        String input ="I am a disco Dancer";
+
+        String reverse="";
+
+        for (int i=0;i<input.length();i++){
+            reverse =input.charAt(i)+reverse;
+        }
+
+        System.out.println(reverse);
+    }
+}
