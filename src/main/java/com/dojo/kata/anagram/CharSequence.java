f2289b9a8d6149d6395ba295d92483ee34b41a21
sreenadh1543@gmail.com <sreenadh1543@gmail.com>
2020-02-28 21:24:28 +0100

    Add max char identifier

--- src/main/java/com/dojo/kata/anagram/CharSequence.java
+++ src/main/java/com/dojo/kata/anagram/CharSequence.java
@@ -14,23 +14,31 @@ public class CharSequence {
         createCharCount(sc.nextLine());
 
 
+
     }
 
 
     private static void createCharCount(String input) {
 
         Map<Character, Integer> charCounter = new HashMap<>();
+        int max = 0;
+        char maxChar = 0;
 
         for (char ch : input.toCharArray()) {
 
             if (charCounter.containsKey(ch)) {
                 int count = charCounter.get(ch);
                 charCounter.put(ch, ++count);
+                if (count > max) {
+                    max = count;
+                    maxChar = ch;
+                }
             } else {
                 charCounter.put(ch, 1);
             }
         }
 
+        System.out.println("Character >>> " + maxChar + " <<< is Used for >>> " + max + " <<< times");
         System.out.println(charCounter.toString());
 
     }
