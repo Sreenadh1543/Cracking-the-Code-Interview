110138a7a3d25af04b4371342d15f28caee4b813
sreenadh1543@gmail.com <sreenadh1543@gmail.com>
2020-10-17 13:47:01 +0200

    Add Solutions to ReverseCharacters problem

--- src/main/java/com/dojo/kata/interviewcake/reversecharacters/ReverseCharacters.java
+++ src/main/java/com/dojo/kata/interviewcake/reversecharacters/ReverseCharacters.java
@@ -1,4 +1,33 @@
 package com.dojo.kata.interviewcake.reversecharacters;
 
+
 public class ReverseCharacters {
+
+    public static void main(String[] args) {
+        char[] charSequence = {'e','s','r','e','v','e','r'};
+        print(charSequence);
+        reverseCharacters(charSequence);
+        print(charSequence);
+    }
+
+    private static void print(char[] charSequence){
+        System.out.println();
+        for (char character:charSequence){
+            System.out.print(character);
+        }
+    }
+
+
+    private static char[] reverseCharacters(char[] charsequence){
+        int lowerBound=0;
+        int upperBound=charsequence.length-1;
+        while (lowerBound<upperBound){
+            char temp = charsequence[lowerBound];
+            charsequence[lowerBound]=charsequence[upperBound];
+            charsequence[upperBound]=temp;
+            ++lowerBound;
+            --upperBound;
+        }
+        return charsequence;
+    }
 }
