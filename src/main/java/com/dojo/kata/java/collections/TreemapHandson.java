c624e849cc016d055bea9425da1561c9e5e6baf1
sreenadh1543@gmail.com <sreenadh1543@gmail.com>
2020-12-22 21:27:14 +0100

    Added TreemapHandson

--- src/main/java/com/dojo/kata/java/collections/TreemapHandson.java
+++ src/main/java/com/dojo/kata/java/collections/TreemapHandson.java
@@ -3,26 +3,57 @@ package com.dojo.kata.java.collections;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.TreeMap;
+
+import static java.lang.Integer.parseInt;
 
 public class TreemapHandson {
 
     public TreeMap<Integer,String> createPlayerPositionMap(String cricketDataset)
     {
+        TreeMap<Integer,String>  playerPosition = new TreeMap<>();
+        String[] temp = splitCricketScoresList(cricketDataset);
 
-        return null;
+        for (String score :temp) {
+            String[] splitScore = score.split("\\,");
+            playerPosition.put(parseInt(splitScore[0]),splitScore[1]);
+        }
+
+        return playerPosition;
 
     }
     public TreeMap<String,Integer> createPlayerScoreMap(String cricketDataset)
     {
+        TreeMap<String,Integer>  playerScoreMap = new TreeMap<>();
+        String[] temp = splitCricketScoresList(cricketDataset);
 
-        return null;
+        for (String score :temp) {
+            String[] splitScore = score.split("\\,");
+            playerScoreMap.put(splitScore[1], parseInt(splitScore[2]));
+        }
 
+        return playerScoreMap;
     }
+
+
     public TreeMap<String,Double> createMatchesMap(String cricketDataset)
     {
+        TreeMap<String,Double>  matchesMap = new TreeMap<>();
+        String[] temp = splitCricketScoresList(cricketDataset);
 
-        return null;
+        for (String score :temp) {
+            String[] splitScore = score.split("\\,");
+            if(matchesMap.containsKey(splitScore[1])){
+                Double value=matchesMap.get((splitScore[1]));
+                value=value+(Double.valueOf(splitScore[2])/Double.valueOf(splitScore[0]));
+                matchesMap.put(splitScore[1],value);
+            }else{
+                matchesMap.put(splitScore[1],(Double.valueOf(splitScore[2])/Double.valueOf(splitScore[0])));
+            }
+        }
+
+        return matchesMap;
     }
 
 
@@ -39,38 +70,84 @@ public class TreemapHandson {
 
         StringBuilder result = new StringBuilder();
 
-        String[] cricketScores;
-        List<String> cricketScoresSplit = new ArrayList<>();
 
-        if(cricketDataset.contains("\\n")){
-            cricketScores=cricketDataset.split("\\n");
-            for (String cricketScore:cricketScores) {
-                 String scores[]=cricketScore.split("\\|");
-                  for (String score:scores) {
-                    cricketScoresSplit.add(score);
-                  }
-            }
-        }else{
-            cricketScores=cricketDataset.split("\\|");
-            for (String cricketScore:cricketScores) {
-                    cricketScoresSplit.add(cricketScore);
-            }
-        }
 
         if("1".equalsIgnoreCase(queryCode[0])){
-            String startPosition=queryCode[1];
-            String endPosition = queryCode[2];
+            int startPosition= parseInt(queryCode[1]);
+            int endPosition = parseInt(queryCode[2]);
+            TreeMap<Integer, String> playerPositionMap = createPlayerPositionMap(cricketDataset);
 
+            playerPositionMap.forEach((key,value)->{
+                if(key>=startPosition && key<=endPosition){
+                    result.append(key).append(" ").append(value).append("\n");
+                }
+            });
 
         }else if("2".equalsIgnoreCase(queryCode[0])){
-            String thresholdScore = queryCode[1];
+            int thresholdScore = parseInt(queryCode[1]);
+            TreeMap<String, Integer> playerScoreMap = createPlayerScoreMap(cricketDataset);
+            TreeMap<String, Integer> filteredPlayers = new TreeMap<>();
+            TreeMap<Integer, String> playerPositionMap = createPlayerPositionMap(cricketDataset);
+            TreeMap<Integer, String> playerAboveThreshold = new TreeMap<>();
+
+            playerScoreMap.forEach((key,value)->{
+                if(value>thresholdScore){
+                    filteredPlayers.put(key,value);
+                }
+            });
+
+            filteredPlayers.forEach((key,value)->{
+                playerPositionMap.forEach((inner_key,inner_value)->{
+                    if(key.equals(inner_value)){
+                        playerAboveThreshold.put(inner_key,inner_value);
+                    }
+                });
+            });
+
+            playerAboveThreshold.forEach((key,value)->{
+                result.append(key).append(" ").append(value).append("\n");
+            });
+
 
         }else if("3".equalsIgnoreCase(queryCode[0])){
+            TreeMap<String, Double> matchesMap = createMatchesMap(cricketDataset);
+            TreeMap<Double,String> sortedMatchAveragedMap = new TreeMap<>();
 
+            matchesMap.forEach((key,value)->{
+                sortedMatchAveragedMap.put(value,key);
+            });
+
+            Map.Entry<Double, String> doubleStringEntry = sortedMatchAveragedMap.lastEntry();
+            result.append("The Efficient Opener is").append(" ").append(doubleStringEntry.getValue());
         }
 
         return result.toString();
     }
+
+    private String[] splitCricketScoresList(String cricketDataset){
+        String[] cricketScores;
+        List<String> cricketScoresSplit = new ArrayList<>();
+        String[] temp = new String[1];
+
+        if(cricketDataset.contains("\n")){
+            cricketScores=cricketDataset.split("\\n");
+            for (String cricketScore:cricketScores) {
+                String scores[]=cricketScore.split("\\|");
+                for (String score:scores) {
+                    cricketScoresSplit.add(score);
+                }
+            }
+        }else{
+            cricketScores=cricketDataset.split("\\|");
+            for (String cricketScore:cricketScores) {
+                cricketScoresSplit.add(cricketScore);
+            }
+        }
+
+        return cricketScoresSplit.toArray(temp);
+    }
+
+
 
 
 
