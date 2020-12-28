4590513e2b33acd258a7ecd5afbfa7c22ec00dd8
sreenadh1543@gmail.com <sreenadh1543@gmail.com>
2020-12-29 00:40:07 +0100

    Added code on lambda

---
+++ src/main/java/com/dojo/kata/java/lambda/LambdaFn.java
@@ -0,0 +1,13 @@
+package com.dojo.kata.java.lambda;
+
+import java.util.*;
+
+public class LambdaFn {
+
+    public List<Long> functionalProgtamming(List<String> listOfIntegers)
+    {
+        //Write your code here
+        return Collections.emptyList();
+    }
+
+}
